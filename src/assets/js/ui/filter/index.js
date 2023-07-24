3bf3bf73cecaa81c8a6fba770dcbce61028c5fce
aisteron <timotheus@list.ru>
2023-07-25 01:00:50 +0300

    w25.07

--- src/assets/js/ui/filter/index.js
+++ src/assets/js/ui/filter/index.js
@@ -1,6 +1,6 @@
 import {load_toast, qs, qsa, xml} from '../../libs';
 import { dx } from './dexie';
-import { store,incremented, decremented } from './store';
+import { store,incremented, decremented, ls } from './store';
 
 export async function Filter(){
 	accordeon()
@@ -8,19 +8,16 @@ export async function Filter(){
 	await dx.load()
 	dx.init()
 	let hash = await get_hash();
+
 	valid(hash) ? draw() : update()
 
 	
-	// Can still subscribe to the store
-	store.subscribe(() => console.log(store.getState()))
 
-	// Still pass action objects to `dispatch`, but they're created for us
+	store.subscribe(() => console.log(store.getState()))
 	store.dispatch(incremented())
-	// {value: 1}
 	store.dispatch(incremented())
-	// {value: 2}
 	store.dispatch(decremented())
-	// {value: 1}
+
 		
 }
 
@@ -52,11 +49,19 @@ async function get_hash(){
 
 }
 
-function valid(hash){}
+function valid(hash){
+	return ls.get() ? true : false
+}
 function draw(){}
-function update(){}
 
+async function update(){
+	let resid = +qs("body").getAttribute("resid")
+	await get_filters(resid)
+}
 
+async function get_filters(){
+	//xml('get_filters',null,'/api/')
+}
 
 
 // redux разобраться
--- src/assets/js/ui/filter/store.js
+++ src/assets/js/ui/filter/store.js
@@ -7,10 +7,6 @@ const counterSlice = createSlice({
   },
   reducers: {
     incremented: state => {
-      // Redux Toolkit allows us to write "mutating" logic in reducers. It
-      // doesn't actually mutate the state because it uses the Immer library,
-      // which detects changes to a "draft state" and produces a brand new
-      // immutable state based off those changes
       state.value += 1
     },
     decremented: state => {
@@ -24,3 +20,11 @@ export const { incremented, decremented } = counterSlice.actions
 export const store = configureStore({
   reducer: counterSlice.reducer
 })
+
+export const ls = {
+	get(){
+		let h = localStorage.getItem('hash')
+		if(!h) return false
+		return h ? JSON.parse(h) : h
+	}
+}
