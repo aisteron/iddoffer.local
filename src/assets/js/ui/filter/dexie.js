edcdff80514cec142c33ea7515bb5abfba627a99
aisteron <timotheus@list.ru>
2023-07-28 16:08:08 +0300

    w28.07

--- src/assets/js/ui/filter/dexie.js
+++ src/assets/js/ui/filter/dexie.js
@@ -74,6 +74,9 @@ export const dx = {
 
 			// запрос к серверу POST → [{resid : editedon }]
 			// особого смысла отправлять editedon нет, но для children мы будет отправлять невалидные хэши
+			
+			// • мы на фронте решаем, что хэши невалидные и можем отправить только id
+			// сервер будет точно знать, что для присланного id категории нужные товары и прочие поля categories[]
 
 			// ответ сервера
 
@@ -86,12 +89,12 @@ export const dx = {
 
 		
 
-		let obj = [{
-			id: resid,
-			editedon: this.ed
-		}]
+		// let obj = [{
+		// 	id: resid,
+		// 	editedon: this.ed
+		// }]
 
-		let res = await xml('get_products',obj,'/api/').then(r => JSON.parse(r));
+		let res = await xml('get_products',[{id: resid}],'/api/').then(r => JSON.parse(r));
 
 		let db = this.init()
 		db.open();
@@ -143,33 +146,33 @@ export const dx = {
 		db.open()
 		let res = await db.cat.where({editedon:this.ed}).toArray()
 		
-		return !res.length ? false : true
+		return !res.length ? (console.log(`%c editedon "${this.ed}" invalid`,"color: #666"),false) : true
 
 	},
 
 	async validate_children(){
+
+		// <script children> loop
+		// если в таблице cat нет записи
+		// или если хэш не совпадает
 		
 		if(!qs('script[children]')) return false
 		let db = this.init()
 		db.open();
 
-		let newRecords = new Set();
+		let newRecords = [];
 
 		for(const item of children){
 			let recordid = await db.cat.where({catid: item.id}).toArray()
 			
-
-			!recordid.length && newRecords.add(item.id)
-
-		
-		
-			if(recordid[0] && recordid[0].editedon){
-				//console.log(recordid[0]?.editedon, item.editedon)
+			if(recordid.length == 0){
+				newRecords.push(item.id)
+			} else {
+				if(recordid[0].editedon !== item.editedon) newRecords.push(item.id)
 			}
-			//(recordid[0]?.editedon !== item.editedon) && newRecords.add(item.id)
 
 		}
-
+		!newRecords.length && console.log(`%c children актуальны`, 'color: #666')
 		await check_deleted_category()
 		return newRecords
 
@@ -177,16 +180,20 @@ export const dx = {
 	},
 
 	async update_children(catids){
+
+		// catid - массив children для обновления их продуктов
+		// возвращает [categories:[], products:[]]
+		// добавляем / удаляем запись в таблице cat
+		// удаляем продукты категорий
+		// добавляем продуты в таблицу prod
 		
 		
 		let db = this.init()
 		db.open();
 
 		let arr = [];
-		children.forEach(el => {
-			catids.has(el.id) && arr.push({id: el.id, editedon: el.editedon})
-		})
-		
+		children.forEach(el => catids.includes(el.id) && arr.push({id: el.id}))
+
 		let res = await xml('get_products',arr,'/api/').then(r => JSON.parse(r))
 
 		// update table cat
@@ -210,10 +217,61 @@ export const dx = {
 		
 		// add products to table prod
 		await db.prod.bulkPut(res.products)
+		console.log(`%c добавил ${res.products.length} товар`, 'color: green')
 
 
 	},
 	async construct_filters(){
+
+		// по id ресурса resid найти в dexie таблице cat конфигурацию фильтра
+
+		// из html тега <script dictionary> взять словарь
+		// сконструировать массив объектов resp для помещения его в таблицу cat в поле filter
+		// resp будет содержать [{ name, label, data['белый','черный'] }]
+
+		// const dict - <script dictionary>
+
+		let resid = +qs("[resid]").getAttribute("resid");
+		let prods = [];
+		let cfg = '';
+		let resp = [];
+		let ids = []; // id категорий или категории - на основе которой будет сконструирован объект фильтра (на основе товаров)
+		
+		let db = this.init()
+		db.open();
+
+		qs('script[children]')
+			? ids = children.map(el => el.id)
+			: ids.push(resid)
+
+		prods = await db.prod.where('catid').anyOf(ids).toArray()
+		if(!prods.length) {console.log(`%c не нашел товары для построения фильтра`,'color: red'); return}
+		
+		cfg = await db.cat.where({catid: resid}).toArray()
+		cfg = cfg[0].cfg.split(",")
+
+
+		cfg.forEach(c => {
+			switch(c){
+				case 'color':
+				case 'brand':
+				case 'material_facade':
+				case 'material_body':
+				case 'material_upholstery':
+					resp.push(default_checkbox(c,prods))
+					break;
+				case 'size':
+					resp.push(size(prods))
+					break;
+				case 'design':
+					resp.push({name: 'design', label: dict['design']})	
+			}
+		})
+
+
+		let recordid = await db.cat.where({catid: resid}).toArray()
+		let response = await db.cat.update(recordid[0].id, {filter: resp})
+		if(response !== 1) console.log('%c ошибка обновления фильтра в категории', 'color: red')
 
 	}
 
@@ -251,4 +309,56 @@ async function check_deleted_category(){
 				.delete()
 		}
 	}
+}
+
+function default_checkbox(config_name, prods){
+	
+	let data = new Set()
+
+
+	prods.forEach(el =>{
+		
+		Array.isArray(el[config_name])
+		? el[config_name].forEach(el => data.add(el))
+		: data.add(el[config_name])
+
+	})
+	
+
+	let obj = {
+		name: config_name,
+		label: dict[config_name],
+		data: Array.from(data)
+	}
+
+	
+	return obj
+	
+
+
+}
+function size(prods){
+	let width = new Set(), height = new Set(), length = new Set();
+	prods.forEach(el => {
+		width.add(el.width)
+		height.add(el.height)
+		length.add(el.length)
+	})
+	
+	let wmin  = Math.min(...width);
+	let wmax  = Math.max(...width);
+
+	let hmin  = Math.min(...height);
+	let hmax  = Math.max(...height);
+
+	let lmin  = Math.min(...length);
+	let lmax  = Math.max(...length);
+
+	return {
+		name: 'size',
+		width: [wmin, wmax],
+		height: [hmin, hmax],
+		length: [lmin, lmax],
+	}
+
 }
--- src/assets/js/ui/filter/index.js
+++ src/assets/js/ui/filter/index.js
@@ -2,6 +2,36 @@ import { load_toast, qs, qsa, xml } from '../../libs';
 import { dx } from './dexie';
 import { store, incremented, decremented } from './store';
 
+/*
+	
+	* есть страница категория категорий
+	* ПРАВИЛО: такие страницы не должны содержать непосредственно товары
+	* признак категории категорий - тег <script children> в <head>
+	
+	* есть страница НЕПОСРЕДСТВЕННО списка товаров
+
+	* обе страницы содержат мета-тег editedon - где хранится дата редактирования страницы
+	* если эта дата отличается от сохраненной в dexie table cat, то мы обновляем инфу
+	* дергаем ф-цию get_products,
+	* которая вернет массив категории(й) с editedon и список фильтров
+	* и массив продуктов
+
+	* если есть <script children> (страница категории категорий)
+	* то мы сравниваем editedon CHILDREN с имеющимися
+	* если отличается, то дергаем ту же функцию get_products
+	* обновляем таблицу cat, а из таблицы prod удаляем все продукты
+	* в таблицу prod загружаем свежие продукты
+
+	* парсим строку фильтров и товары категории(й)
+	* создаем объект filter со значениями основанными на значениях продуктов
+	
+	* функция draw() читает таблицу cat, поле filter по resource id
+	* и рисует строку фильтров
+
+	* осталось повесить слушатели и диспатчить в состояние объект фильтров
+	* по которому будем выполнять фильтрацию dexie таблицы prod
+
+*/
 export async function Filter() {
 
 	await dx.load()
@@ -10,13 +40,13 @@ export async function Filter() {
 	!ed && await dx.update_editedon()
 
 
-	let ch = qs('script[children]')
-	ch && await dx.validate_children()
-					.then(resp => resp.size && dx.update_children(resp))
-					//.then(resp => console.log(resp))
+	if(qs('script[children]')){
+		let resp = await dx.validate_children()
+		resp.length && dx.update_children(resp)
+	}
 
-	// await dx.construct_filters()
-	//draw()
+	await dx.construct_filters()
+	draw()
 
 
 
@@ -42,15 +72,16 @@ function accordeon() {
 
 
 async function draw() {
+
 	qs("#filter").classList.remove("loading")
 
 	let resid = +qs('[resid]').getAttribute("resid")
 	let db = dx.init();
 	let obj = await db.cat.where({ catid: resid }).toArray();
 	let str = ``
-
+	
 	obj[0].filter.forEach(f => {
-
+		
 		switch(f.name){
 			case 'color':
 			case 'brand':
@@ -58,6 +89,12 @@ async function draw() {
 			case 'material_body':
 			case 'material_upholstery':
 				str += template_checkbox(f);
+				break;
+			case 'size':
+				str += template_size(f);
+				break;
+			case 'design':
+				str += template_design(f);
 				break;
 		}
 
@@ -76,19 +113,66 @@ function template_checkbox(f){
 	let str = ``
 
 	str += `<div class="item dd" data-name="${f.name}">
-										<span>${f.label}</span><ul>`
-			f.data.forEach(c => {
-				str += `
-						<li>
-							<label>
-								<input type="checkbox">
-								<span class="mark"></span>
-								<span class="name">${c}</span>
-							</label>
-						</li>`
-			})
-			str += `</ul></div>`
-	return str		
+			     <span>${f.label}</span><ul>`
+						f.data.forEach(c => {
+							str += `
+									<li>
+										<label>
+											<input type="checkbox">
+											<span class="mark"></span>
+											<span class="name">${c}</span>
+										</label>
+									</li>`
+						})
+	str += `</ul></div>`
+	
+	return str
+
+}
+
+function template_size(f){
+	let str = `
+			<div class="item dd" data-name="size">
+				<span>${dict['size']}</span>
+				<div class="size">
+					<span>Ширина, мм</span>
+					<div class="row">
+						<input type="number" min="${f.width[0]}" placeholder="${f.width[0]}" />
+						<input type="number" max="${f.width[1]}" placeholder="${f.width[1]}" />
+					</div>
+				</div>	
+				
+				<div class="size">	
+					<span>Длина, мм</span>
+					<div class="row">
+						<input type="number" min="${f.length[0]}" placeholder="${f.length[0]}" />
+						<input type="number" max="${f.length[1]}" placeholder="${f.length[1]}" />
+					</div>
+				</div>
+				
+				<div class="size">	
+					<span>Высота, мм</span>
+					<div class="row">
+						<input type="number" min="${f.height[0]}" placeholder="${f.height[0]}" />
+						<input type="number" max="${f.height[1]}" placeholder="${f.height[1]}" />
+					</div>
+				</div>
+			</div>`
+
+	return str;
+	
+}
+
+function template_design(f){
+	return `
+		<div class="item design">
+			<label>
+				<input type="checkbox" />
+				<span class="mark"></span>
+				<span class="name">${dict['design']}</span>
+			</label>
+		</div>
+	`
 
 }
 
