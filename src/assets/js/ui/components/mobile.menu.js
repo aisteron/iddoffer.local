d922e32902621872d34c61db5e3a66ec2198bd91
aisteron <timotheus@list.ru>
2023-08-29 22:14:11 +0300

    mobile menu

--- src/assets/js/ui/components/mobile.menu.js
+++ src/assets/js/ui/components/mobile.menu.js
@@ -6,5 +6,13 @@ export function mobile_menu(){
 function open(){
 	qs("#nav-icon1").addEventListener("click", event => {
 		event.target.classList.toggle("open")
+		qs('#mobile_menu').classList.toggle("open")
+	})
+
+	document.addEventListener("click", event => {
+		if(event.target == qs("#nav-icon1")) return
+		if(qs('#mobile_menu').contains(event.target)) return
+		qs('#mobile_menu').classList.remove("open")
+		qs("#nav-icon1").classList.remove('open')
 	})
 }
